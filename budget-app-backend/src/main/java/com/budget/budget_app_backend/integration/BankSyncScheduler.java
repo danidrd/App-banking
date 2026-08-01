c30f8653cd5fb4ab227382776370c3d8b3da67e7
danidrd <danielecaliandro.dev@gmail.com>
2026-08-01 17:44:03 +0200

    BankSyncScheduler updated at hours 6:00 and 18:00

--- budget-app-backend/src/main/java/com/budget/budget_app_backend/integration/BankSyncScheduler.java
+++ budget-app-backend/src/main/java/com/budget/budget_app_backend/integration/BankSyncScheduler.java
@@ -29,7 +29,7 @@ public class BankSyncScheduler {
         this.bankSyncService = bankSyncService;
     }
 
-    @Scheduled(cron = "0 0 11,18 * * *")
+    @Scheduled(cron = "0 0 6,18 * * *")
     public void syncAllConnectedAccounts() {
         List<Account> connected = accountRepository.findByExternalUidIsNotNull();
         log.info("Sincronizzazione automatica avviata per {} conti collegati", connected.size());
